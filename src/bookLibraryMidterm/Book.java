e36746bcd6278df378cdc8f877f2cb6c8fe20b68
Lindsay Karty <lmkarty@gmail.com>
2018-10-25 16:18:14 -0400

    added getters and setters to book class

--- src/bookLibraryMidterm/Book.java
+++ src/bookLibraryMidterm/Book.java
@@ -2,7 +2,38 @@ package bookLibraryMidterm;
 
 public class Book {
 	
-	public static void test() {
-		System.out.println("Change this once we start project");
+	private int bookID;
+	private String bookTitle;
+	private String bookAuthor;
+	private String bookStatus;
+	private String bookDue;
+
+	public int getBookID() {
+		return bookID;
 	}
+
+	public String getBookTitle() {
+		return bookTitle;
+	}
+
+	public String getBookAuthor() {
+		return bookAuthor;
+	}
+
+	public String getBookStatus() {
+		return bookStatus;
+	}
+
+	public String getBookDue() {
+		return bookDue;
+	}
+
+	public void setBookStatus(String bookStatus) {
+		this.bookStatus = bookStatus;
+	}
+
+	public void setBookDue(String bookDue) {
+		this.bookDue = bookDue;
+	}
+
 }
