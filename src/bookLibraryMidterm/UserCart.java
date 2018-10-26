5497476cc247544bc222bce990e015af038a5048
Carl <cjones6343@gmail.com>
2018-10-26 14:37:39 -0400

    Started foundation for how cart should function

--- src/bookLibraryMidterm/UserCart.java
+++ src/bookLibraryMidterm/UserCart.java
@@ -1,8 +1,26 @@
 package bookLibraryMidterm;
 
+import java.util.HashMap;
+import java.util.Iterator;
+
 public class UserCart {
-	public void setCart() {
+	//these arrays should be inherited from inventory class
+	HashMap <String, String> chosenBooks = new HashMap<>();
+	HashMap <String, String> userCart = new HashMap<>();
+	
+	public void setCart(String bookAuthor, String bookTitle) {
+		for (int i = 0; i < chosenBooks.size(); i++) {
+			userCart.put(bookTitle, bookTitle);
+		}
 		
 	}
-	public void cartCheck
+	public String cartCheckout() {
+		String myOrder = null;
+		
+		for (String browse : chosenBooks.values()) {
+			myOrder = browse;
+		}
+		return myOrder;
+		//should return all books within cart hashmap as strings to display a final cart to user
+	}
 }
