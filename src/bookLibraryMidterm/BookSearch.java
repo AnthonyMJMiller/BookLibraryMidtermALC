f01b601eb37d7338c3ab4a9571c03db650ae1673
Anthony <anthonymiller313@hotmail.com>
2018-10-29 11:44:43 -0400

    removed test main. search is functioning

--- src/bookLibraryMidterm/BookSearch.java
+++ src/bookLibraryMidterm/BookSearch.java
@@ -7,34 +7,7 @@ public class BookSearch {
 	static ArrayList<Book> chosenBook = new ArrayList<>();
 	static Scanner scnr = new Scanner(System.in);
 
-	public static void main(String[] args) {
 
-		for (int i = 0; i < BookInventory.bookArray().size(); i++) {
-			System.out.println(BookInventory.bookArray().get(i));// test information
-		}
-		int testSelect = Validator.getInt(scnr, "Pick one or two: (test)");
-		if (testSelect == 1) {
-			searchByAuthor();
-		} else if (testSelect == 2) {
-			searchByTitle();
-		} else { 
-			System.out.println("Please select one or two.");
-		}
-//		System.out.println("Add to cart? (Y/N)");
-//
-//		String cartAdd = scnr.nextLine();
-//
-//		if (cartAdd.equalsIgnoreCase("Y")) {
-//			// pass in status of book to be able to determine if it can be checked out if so
-//			// add current book information to usercart list-type variable
-//		} else if (cartAdd.equalsIgnoreCase("N")) {
-//			System.out.println("Okay, returning to home...");
-//		} else {
-//			System.out.println("Sorry, search could not be completed - Contact Help Desk...");
-//			
-//		}
-//		scnr.close();
-	}
 
 	public BookSearch() {
 
